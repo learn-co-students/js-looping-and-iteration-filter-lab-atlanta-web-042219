a164677496acea975089e295b94a52c6391e824a
Andrew Allen <asallen67@gmail.com>
2019-06-03 15:41:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(arr, term) {
+	return arr.filter(ele => ele.toLowerCase() === term.toLowerCase())
+}
+
+function fuzzyMatch(arr, term) {
+	return arr.filter(ele => ele.slice(0,term.length) === term)
+}
+
+function matchName(obj_arr, term) {
+	return obj_arr.filter(obj => obj.name === term)
+}
